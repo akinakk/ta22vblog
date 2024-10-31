86168edd1dc4b2184bfbd9054bd7bd98a1539ef0
akinakk <artemi.baburin@tptlive.ee>
2024-10-31 13:14:52 +0300

    Reset password page rewrite in daisyui

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+    <div class="mb-4 text-sm text-white">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -10,16 +10,25 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <label class="form-control w-full mb-4">
+            <div class="label">
+                <span class="label-text text-white">Email</span>
+            </div>
+            <input id="email" name="email" type="email" placeholder="Enter your email"
+                class="input input-bordered @error('email') input-error @enderror w-full text-white" required autofocus
+                value="{{ old('email') }}" />
+            <div class="label">
+                @error('email')
+                    <span class="label-text-alt text-error">{{ $message }}</span>
+                @enderror
+            </div>
+        </label>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button type="submit" class="btn-primary btn" style="background-color: oklch(75.4611% 0.18307 346.812432 / 1);">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
+
     </form>
 </x-guest-layout>
